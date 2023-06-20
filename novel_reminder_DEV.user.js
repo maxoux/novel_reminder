6794f6f38f23050c167d37047972e8b85cc31594
maxoux <maxoux@example.com>
2023-06-20 12:15:09 +0200

    Feat: Add dev mode

---
+++ novel_reminder_DEV.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         Novel Reminder
+// @match        DISABLED_https://www.webnovel.com/book/*/*
+// @match        https://*boxnovel.com/novel/*/*
+// @match        http://readfreenovel.com/*/*
+// @match        https://wuxiaworld.site/novel/*
+// @match        https://daonovel.com/novel/*
+//
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @require      https://cdn.jsdelivr.net/npm/vue/dist/vue.js
+// @require      https://unpkg.com/uuid@latest/dist/umd/uuidv4.min.js
+// @require      https://kit.fontawesome.com/4d41395a13.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.15/lodash.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/randomcolor/0.6.1/randomColor.min.js
+// @require      https://cdn.jsdelivr.net/npm/vue-js-modal@1.3.28/dist/index.min.js
+// @require      http://localhost:8080/novel_reminder.user.js
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+// Temporary holded library for future use (maybe)
+// @require      https://cdnjs.cloudflare.com/ajax/libs/spectrum/1.8.1/spectrum.min.js
+// @resource     spec https://cdnjs.cloudflare.com/ajax/libs/spectrum/1.8.1/spectrum.min.css
+
+
+
+(function() {
+})();
